4b9fa0652ae3c8c23032991c63e65ba0a19c5653
sascha iseli <sascha.iseli@gmx.ch>
2019-05-22 22:24:20 +0200

    build & push

--- push.sh
+++ push.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 echo "Push Docker Image to DockerHub"
-docker push iselisa/wildfly:$1 --username $2 --password $3
+docker login  --username $2 --password $3
+docker push iselisa/wildfly:$1
